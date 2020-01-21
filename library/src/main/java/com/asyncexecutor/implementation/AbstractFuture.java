e6940785fe46be4eed87d3f5532401b56b1e3d8d
Pavel Sokolov <pavel.zeppa@yandex.ru>
2020-01-21 15:53:51 +0300

    New class is added - AbstractFuture
    - base class that combines AbstractObservable and Future

---
+++ library/src/main/java/com/asyncexecutor/implementation/AbstractFuture.java
@@ -0,0 +1,289 @@
+package com.asyncexecutor.implementation;
+
+import com.asyncexecutor.Consumer;
+import com.asyncexecutor.ObservableFuture;
+import com.asyncexecutor.Observer;
+import com.asyncexecutor.Subscription;
+import com.asyncexecutor.Transformer;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Base class that implement {@link ObservableFuture<T>} interface and extends {@link AbstractObservable<T>}.
+ * It emits single value or stream of values depending on future type. Methods like {@link AbstractFuture#get()}
+ * return last emitted value.
+ *
+ * @param <T> type of value to be emitted
+ */
+public abstract class AbstractFuture<T> extends AbstractObservable<T> implements ObservableFuture<T> {
+    private final boolean isMultiCompletable;
+    protected volatile T result;
+    protected volatile Exception exception;
+    private volatile State state = State.INITIAL;
+
+    protected AbstractFuture(Executor executor, boolean isMultiCompletable) {
+        super(executor);
+        this.isMultiCompletable = isMultiCompletable;
+    }
+
+    protected AbstractFuture(Executor executor) {
+        this(executor, false);
+    }
+
+    /**
+     * Complete current {@link AbstractFuture<T>} with provided result. {@link AbstractFuture<T>} can
+     * be completed multiple times in case it is created as multi completable.
+     *
+     * @param result value or null
+     * @return true if future was successfully completed or false if it has been already completed or
+     * canceled
+     */
+    @Override
+    public synchronized boolean complete(T result) {
+        return emit(result);
+    }
+
+    /**
+     * Complete current {@link AbstractFuture<T>} with provided error. {@link AbstractFuture<T>} can
+     * be completed multiple times in case it is created as multi completable.
+     *
+     * @param exception {@link Exception} instance
+     * @return true if future was successfully completed or false if it has been already completed or
+     * canceled
+     */
+    @Override
+    public synchronized boolean complete(Exception exception) {
+        return emit(exception);
+    }
+
+    @Override
+    public boolean isMultiCompletable() {
+        return isMultiCompletable;
+    }
+
+    /**
+     * This method cancels current {@link AbstractFuture<T>}. Canceled future cannot be completed.
+     * If there is computation in progress then it also can be terminated.
+     *
+     * @param mayInterruptIfRunning terminate result computation if it is true. In most cases it means
+     *                              that current {@link Thread} is interrupted {@link Thread#interrupt}.
+     * @return true if future has been successfully canceled
+     */
+    @Override
+    public synchronized boolean cancel(boolean mayInterruptIfRunning) {
+        if (setState(State.CANCELED)) {
+            removeAllSubscribers();
+            notifyAll();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return state == State.CANCELED;
+    }
+
+    @Override
+    public boolean isDone() {
+        return state == State.COMPLETED;
+    }
+
+    /**
+     * This method waits for the result. Calling {@link Thread} is blocked until {@link AbstractFuture<T>}
+     * is completed or canceled.
+     *
+     * @return result
+     * @throws ExecutionException   if error has been thrown during computation
+     * @throws InterruptedException when operation has been interrupted
+     */
+    @Override
+    public synchronized T get() throws ExecutionException, InterruptedException {
+        if (state == State.COMPLETED) {
+            return getResultOrException();
+        } else if (state != State.CANCELED) {
+            wait();
+            if (state == State.COMPLETED) {
+                return getResultOrException();
+            }
+        }
+        throw new CancellationException();
+    }
+
+
+    /**
+     * This method waits for the result. Calling {@link Thread} is blocked until {@link AbstractFuture<T>}
+     * is completed, canceled or time is out.
+     *
+     * @param timeout value that represents duration
+     * @param unit    specifies time units to measure duration
+     * @return
+     * @throws ExecutionException   when error has been thrown during computation
+     * @throws InterruptedException when operation has been interrupted
+     * @throws TimeoutException     when time is out
+     */
+    @Override
+    public synchronized T get(long timeout, TimeUnit unit) throws ExecutionException, InterruptedException, TimeoutException {
+        if (state == State.COMPLETED) {
+            return getResultOrException();
+        } else if (state != State.CANCELED) {
+            long nanos = unit.toNanos(timeout);
+            wait(nanos / 1000, (int) (nanos % 1000));
+            if (state == State.COMPLETED) {
+                return getResultOrException();
+            } else if (state != State.CANCELED) {
+                throw new TimeoutException();
+            }
+        }
+        throw new CancellationException();
+    }
+
+    @Override
+    public T getResult() {
+        try {
+            return get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized Subscription<T> accept(Executor executor, Consumer<T> consumer) {
+        Subscriber subscriber = addSubscriber(executor, consumer);
+        if (state == State.COMPLETED) {
+            subscriber.accept(result, exception);
+        }
+        return subscriber;
+    }
+
+    @Override
+    public synchronized Subscription<T> observe(Executor executor, Observer<T> observer) {
+        Subscriber subscriber = addSubscriber(executor, observer);
+        if (state == State.COMPLETED) {
+            subscriber.accept(result, exception);
+        }
+        return subscriber;
+    }
+
+    @Override
+    public <D> ObservableFuture<D> map(Executor executor, Transformer<T, D> transformer) {
+        TransformingFuture<D, T> future = new TransformingFuture<>(this, executor, isMultiCompletable, transformer);
+        observe(executor, future);
+        return future;
+    }
+
+    @Override
+    public <D> ObservableFuture<D> map(Transformer<T, D> transformer) {
+        return map(getExecutor(), transformer);
+    }
+
+    /**
+     * This methods emits new value to stream and changes current {@link AbstractFuture<T>} state to
+     * {@link State#COMPLETED}. If current future is multi completable this method can be called
+     * multiple times to change last emitted value.
+     *
+     * @param result value to be emitted
+     * @return true if future has been successfully completed
+     */
+    @Override
+    protected synchronized final boolean emit(T result) {
+        if (setState(State.COMPLETED)) {
+            this.result = result;
+            this.exception = null;
+            emit(result, exception);
+            notifyAll();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This methods emits error to stream and changes current {@link AbstractFuture<T>} state to
+     * {@link State#COMPLETED}. If current future is multi completable this method can be called
+     * multiple times to change last emitted value.
+     *
+     * @param e {@link Exception} instance
+     * @return true is future has been successfully completed
+     */
+    @Override
+    protected synchronized final boolean emit(Exception e) {
+        if (setState(State.COMPLETED)) {
+            this.exception = e;
+            emit(null, e);
+            notifyAll();
+            return true;
+        }
+        return false;
+    }
+
+    protected State getState() {
+        return state;
+    }
+
+    /**
+     * This method tries to change a state of current {@link AbstractFuture<T>}. It limits possible
+     * transitions. If current state is {@link State#COMPLETED} it can not be changed to
+     * {@link State#CANCELED} for example.
+     *
+     * @param newState state to be applied
+     * @return true is state was changed, false otherwise
+     */
+    protected synchronized final boolean setState(State newState) {
+        if (state == State.INITIAL) {
+            if (newState == State.COMPLETING || newState == State.COMPLETED ||
+                    newState == State.CANCELED) {
+                state = newState;
+                return true;
+            }
+        } else if (state == State.COMPLETING) {
+            if (newState == State.COMPLETED || newState == State.CANCELED) {
+                state = newState;
+                return true;
+            }
+        } else if (state == State.COMPLETED && isMultiCompletable) {
+            if (newState == State.INITIAL || newState == State.COMPLETED) {
+                state = newState;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private T getResultOrException() throws ExecutionException {
+        if (exception == null) {
+            return result;
+        } else {
+            throw new ExecutionException(exception);
+        }
+    }
+
+    /**
+     * Possible states
+     */
+    protected enum State {
+        /**
+         * Initial state that is assigned to {@link AbstractFuture} when it has been created
+         */
+        INITIAL,
+
+        /**
+         * This state is assigned when computation is started
+         */
+        COMPLETING,
+
+        /**
+         * This state is assigned when computation is finished, value or error has been emitted
+         */
+        COMPLETED,
+
+        /**
+         * This state is assigned when {@link AbstractFuture} is canceled. It is terminal state
+         * that can not be changed.
+         */
+        CANCELED
+    }
+}
---
+++ library/src/main/java/com/asyncexecutor/implementation/TransformingFuture.java
@@ -0,0 +1,53 @@
+package com.asyncexecutor.implementation;
+
+import com.asyncexecutor.ObservableFuture;
+import com.asyncexecutor.Observer;
+import com.asyncexecutor.Transformer;
+
+import java.util.concurrent.Executor;
+
+/**
+ * This class implements a phase that processes values emitted by another {@link AbstractFuture}.
+ *
+ * @param <D> type of values to be re-emitted
+ * @param <T> type of values emitted by parent
+ */
+class TransformingFuture<D, T> extends AbstractFuture<D> implements Observer<T> {
+    private final AbstractFuture<T> parent;
+    private final Transformer<T, D> transformer;
+
+    TransformingFuture(AbstractFuture<T> parent, Executor executor, boolean isMultiCompletable, Transformer<T, D> transformer) {
+        super(executor, isMultiCompletable);
+        this.parent = parent;
+        this.transformer = transformer;
+    }
+
+    @Override
+    public synchronized boolean cancel(boolean mayInterruptIfRunning) {
+        if (super.cancel(mayInterruptIfRunning)) {
+            return parent.cancel(mayInterruptIfRunning);
+        }
+        return false;
+    }
+
+    @Override
+    public ObservableFuture<D> submit() {
+        parent.submit();
+        return this;
+    }
+
+    /**
+     * This method handle values and errors that are emitted by parent {@link AbstractFuture}
+     *
+     * @param t value to be processed
+     * @param e exception that is not null in case of operation completed with error
+     */
+    @Override
+    public void accept(T t, Exception e) {
+        try {
+            emit(transformer.transform(t, e));
+        } catch (Exception exception) {
+            emit(exception);
+        }
+    }
+}
